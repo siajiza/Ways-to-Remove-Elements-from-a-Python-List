199be2fa33a8c5b58e3fbdc4008c86f41b4b3e4c
Siajiza <>
2021-04-11 11:14:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# List´s created, so could be exist changes like remove, includ or sustituve. [ ]
+
+print('-----Inicial List')
+
+List_user = ['Ana', 'Paz', 'Lola'] # These are the new users List.
+
+print(List_user) # Inicial List ['Ana', 'Paz', 'Lola']
+
+print('-----List 1º change')
+
+List_user.insert(0, 'Max') # we add a new element on the position zero. 
+
+print(List_user) # List 1º change ['Max', 'Ana', 'Paz', 'Lola']
+
+print('-----List 2º change')
+
+List_user.append('Teo') # we add a new element at the end of the list.
+
+print(List_user) # List 2º change ['Max', 'Ana', 'Paz', 'Lola']
+
+print('-----List 3º change')
+
+List_user[1] = 'Leo' # we must sustitue the exinting element called "Ana" for a correct item called "Leo".
+
+print(List_user) # List 3º change ['Max', 'Leo', 'Paz', 'Lola', 'Teo']
+
+
+# Now we must remove the element []
+
+print('-----List 4º change')
+
+List_user.remove('Paz') # we remove the element from the List_user.
+
+print(List_user)
+
+# Now we popped(remove) the very last element
+# with pop() but we don-t only remove we can use this items to another varible, so this "pop()" is going to gives us the avility to use this items on another place.
+
+print('-----List 5º change')
+
+popped_List_user = List_user.pop()
+
+print('-----popped-----')
+print(popped_List_user)
+
+print('-----removed-----')
+print(List_user)
+
+
+
